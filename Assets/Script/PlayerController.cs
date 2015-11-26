b4fff111d81b325d0c65b5467308e22efcc96569
Edmund_chi <axdc11239@gmail.com>
2015-11-26 15:32:59 +0800

    complete turtorial 2/8 Moving the Player

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	// Use this for initialization
+	private Rigidbody rb;
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce(movement*speed);
+	
+	}
+}
